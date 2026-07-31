ce41a7b4f1ab5de8c3b342e9c642cce7859cc223
miyori-knzk <miyu.a_1022@icloud,com>
2026-07-31 14:05:42 +0900

    20260731_カテゴリーCRUD作成

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +20,5 @@ Route::get('/', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/tasks', fn () => 'タスク一覧（準備中）')->name('tasks.index');
-    Route::get('/categories', fn () => 'カテゴリー一覧（準備中）')->name('categories.index');
+    Route::resource('/categories', CategoryController::class);
 });
