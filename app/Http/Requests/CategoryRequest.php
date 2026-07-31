7ddebb25f4180f76a082922abb2f47d9596dccc9
miyori-knzk <miyu.a_1022@icloud,com>
2026-07-31 15:18:11 +0900

    20260731_タスクCRUD作成

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -13,7 +13,7 @@ class CategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,6 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/tasks', fn () => 'タスク一覧（準備中）')->name('tasks.index');
     Route::resource('/categories', CategoryController::class);
+    Route::resource('/tasks', TaskController::class);
 });
